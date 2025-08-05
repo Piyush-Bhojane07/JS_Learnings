e3fac4fa4790999d04d5bee682ef8c69e28a4bd1
Piyush-Bhojane07 <piyush.bhojane00@gmail.com>
2025-08-05 20:42:18 +0530

    leap year with changes

--- DSA/01_Basics/LeapYear.js
+++ DSA/01_Basics/LeapYear.js
@@ -1,8 +1,12 @@
+const readline = require("readline-sync");
+
+const year = parseInt(readline.question("Enter a year: "));
+
 function leap(year) {
-  if (year % 4 === 0) {
-    return result;
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return "Leap Year";
   }
-  return -1;
+  return "Not a Leap Year";
 }
 
-console.log(leap(5));
+console.log(leap(year));
