f7e5404e586387e0178a9cfed541eda669ce3690
Piyush-Bhojane07 <piyush.bhojane00@gmail.com>
2025-08-08 00:00:07 +0530

    changes

--- DSA/01_Basics/Table.js
+++ DSA/01_Basics/Table.js
@@ -3,9 +3,11 @@ let input = parseInt(readline.question("Enter a number"))
 
 function table (tableOf){
     let tableOfArr = []
+    
     for(let i = 1; i <= 10; i++){
         tableOfArr[i-1] = i * tableOf;
     }
+
     return tableOfArr;
 }
 console.table(table(input))
--- DSA/01_Basics/TillX.js
+++ DSA/01_Basics/TillX.js
@@ -1,17 +1,22 @@
 const readline = require("readline-sync");
 
 let sum = 0;
+
 while (true) {
+
     let input = readline.question("Enter a number (or 'x' to stop): ");
     if (input.toLowerCase() === 'x') {
         break;
     }
-    let number = parseInt(input);
+
+    let number =  parseInt(input);
+
     if (!isNaN(number)) {
         sum += number;
     } else {
         console.log("Invalid input, please enter a valid number or 'x' to exit.");
     }
+    
 }
 
 console.log("Total sum:", sum);
