f1d453c98265ec1457ada19fcde9a452989bd631
Piyush-Bhojane07 <piyush.bhojane00@gmail.com>
2025-08-12 10:32:23 +0530

    palindrome program

--- DSA/02_conditions/Intermediate/NcpNpr.js
+++ DSA/02_conditions/Intermediate/NcpNpr.js
@@ -1,1 +1,25 @@
 //Find Ncr & Npr
+
+const readline = require("readline-sync");
+
+// Factorial function
+function factorial(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+
+let n = parseInt(readline.question("Enter value of n: "));
+let r = parseInt(readline.question("Enter value of r: "));
+
+if (r > n) {
+  console.log("r should be less than or equal to n");
+} else {
+  let nCr = factorial(n) / (factorial(r) * factorial(n - r));
+  let nPr = factorial(n) / factorial(n - r);
+
+  console.log(`nCr = ${nCr}`);
+  console.log(`nPr = ${nPr}`);
+}
