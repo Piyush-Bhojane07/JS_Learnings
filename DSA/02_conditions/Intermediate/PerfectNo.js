2dbdada05657a8291a4285f1ce6dea90a10be7af
Piyush-Bhojane07 <piyush.bhojane00@gmail.com>
2025-08-12 10:52:53 +0530

    perfect no

--- DSA/02_conditions/Intermediate/PerfectNo.js
+++ DSA/02_conditions/Intermediate/PerfectNo.js
@@ -1,1 +1,16 @@
-//Perfect Number
+const readline = require("readline-sync");
+let num = parseInt(readline.question("Enter a number: "));
+
+let sum = 0;
+for (let i = 1; i <= num / 2; i++) {
+  // start from 1, go till num/2
+  if (num % i === 0) {
+    sum += i;
+  }
+}
+
+if (sum === num && num !== 0) {
+  console.log("Perfect number");
+} else {
+  console.log("Not a perfect number");
+}
