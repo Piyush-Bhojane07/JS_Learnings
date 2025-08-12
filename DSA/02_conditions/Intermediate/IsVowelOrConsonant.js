b3da314bc3c9ff05d6b7b9c1cac594f1377f2ef8
Piyush-Bhojane07 <piyush.bhojane00@gmail.com>
2025-08-12 10:40:31 +0530

    is Vowels or consonnants

--- DSA/02_conditions/Intermediate/IsVowelOrConsonant.js
+++ DSA/02_conditions/Intermediate/IsVowelOrConsonant.js
@@ -1,1 +1,18 @@
 //Program Vowel Or Consonant
+const readline = require("readline-sync");
+let input = readline.question("enter char").toLowerCase();
+// if(input === 'a' || input === 'e'|| input === 'i'|| input === 'o'|| input === 'u'){
+//     console.log("vowels")
+// }else {console.log("Consonants")}
+
+function isVowelsConsonants() {
+  if (input.length !== 1 || !/[a-z]/.test(input)) {
+    console.log("Please enter a single alphabet character.");
+  } else if ("aeiou".includes(input)) {
+    console.log("Vowel");
+  } else {
+    console.log("Consonant");
+  }
+}
+
+isVowelsConsonants();
